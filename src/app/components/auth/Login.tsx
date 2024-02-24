9f093c3afac1508ab8aa7b685f6d9190186688b1
Drake Sorensen <>
2024-02-24 00:47:46 -0600

    Updated callback URL

--- src/app/components/auth/Login.tsx
+++ src/app/components/auth/Login.tsx
@@ -17,7 +17,7 @@ const Login = (props: Props) => {
       username: userName.current,
       password: pass.current,
       redirect: true,
-      callbackUrl: props.callbackUrl ?? "loras-baja.vercel.app",
+      callbackUrl: props.callbackUrl ?? "https://loras-baja.vercel.app",
     });
   };
   return (
